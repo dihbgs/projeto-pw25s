b500d56fa78b54ccffab5d77bbf26d4242f85a49
dihbgs <dihborgesdr@gmail.com>
2023-09-13 00:40:28 -0300

    fix package name

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/ServerApplication.java
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package br.edu.utfpr.pb.pw25s.server.server;
+package br.edu.utfpr.pb.pw25s.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
