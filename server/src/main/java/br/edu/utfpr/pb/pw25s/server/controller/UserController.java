fda6a833f97905741aaac7f68d2e8f04acbd02d4
dihbgs <dihborgesdr@gmail.com>
2023-09-18 22:44:46 -0300

    chore: created generic response and user controller

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/controller/UserController.java
@@ -0,0 +1,32 @@
+package br.edu.utfpr.pb.pw25s.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
+
+import br.edu.utfpr.pb.pw25s.server.model.User;
+import br.edu.utfpr.pb.pw25s.server.service.UserService;
+import br.edu.utfpr.pb.pw25s.server.shared.GenericResponse;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public GenericResponse createUser(@Valid @RequestBody User user) {
+        userService.save(user);
+
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setMessage("User succesfully saved!");
+
+        return genericResponse;
+    }
+}
