26c760590f80c3b4e2acfc39f3e96b551bfbc6d6
dihbgs <dihborgesdr@gmail.com>
2023-10-02 22:36:15 -0300

    chore: create category model

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/Category.java
@@ -0,0 +1,24 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+@EqualsAndHashCode(callSuper = false)
+public class Category extends GenericModel {
+    @NotNull
+    @Size(min = 2, max = 50)
+    @EqualsAndHashCode.Exclude
+    @Column(length = 50, nullable = false)
+    private String name;
+}
